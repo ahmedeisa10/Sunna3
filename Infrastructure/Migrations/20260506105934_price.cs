23a096c2fad35a7d3df9660b6814a840254fda70
Ahmed Noser <ahmednoseer2004@gmail.com>
2026-05-06 14:00:21 +0300

    add price

---
+++ Infrastructure/Migrations/20260506105934_price.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tamkeen.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class price : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "Tickets",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Tickets");
+        }
+    }
+}
